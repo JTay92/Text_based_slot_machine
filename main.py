4525bf55e4e3aa4a9b7d1899a649c11634a56ee6
JTay92 <joe.r.tayler@outlook.com>
2023-03-25 17:38:12 +0000

    added function to get the number of lines to be bet on

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+MAX_LINES = 3   # constant value - caps letters std Python convention
 
 def deposit():
     while True: # Loop runs until valid number is provided
@@ -13,7 +14,24 @@ def deposit():
     
     return amount
 
+def get_number_of_lines():
+        while True: # Loop runs until valid number is provided
+            lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+            if lines.isdigit():    # Check inputted amount is a number
+                lines = int(lines)    # Convert amount from string to integer
+                if 1 <= lines <= MAX_LINES: # Check no. lines within range
+                    break
+                else:
+                    print("Enter a valid number of lines.")
+            else:
+                print("Please enter a number.")
+        
+        return lines
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
 
 main()
