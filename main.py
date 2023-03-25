55d8537aa1247c01ff45373ea703c1fc634cba98
JTay92 <joe.r.tayler@outlook.com>
2023-03-25 17:47:45 +0000

    added get_number_of_lines and get_bet functions and updated main function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3   # constant value - caps letters std Python convention
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True: # Loop runs until valid number is provided
@@ -15,23 +17,38 @@ def deposit():
     return amount
 
 def get_number_of_lines():
-        while True: # Loop runs until valid number is provided
-            lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
-            if lines.isdigit():    # Check inputted amount is a number
-                lines = int(lines)    # Convert amount from string to integer
-                if 1 <= lines <= MAX_LINES: # Check no. lines within range
-                    break
-                else:
-                    print("Enter a valid number of lines.")
+    while True: # Loop runs until valid number is provided
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():    # Check inputted amount is a number
+            lines = int(lines)    # Convert amount from string to integer
+            if 1 <= lines <= MAX_LINES: # Check no. lines within range
+                break
             else:
-                print("Please enter a number.")
-        
-        return lines
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    
+    return lines
 
+def get_bet():
+    while True: # Loop runs until valid number is provided
+        amount = input("What would you like to bet on each line? £ ")
+        if amount.isdigit():    # Check inputted amount is a number
+            amount = int(amount)    # Convert amount from string to integer
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between £{MIN_BET} - £{MAX_BET}.")   #f string only possible in Python 3.
+        else:
+            print("Please enter a number.")
+    
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    total_bet = lines * bet # Valiable to calculate total bet.
+    print(f"You are betting £{bet} on {lines} lines. Total bet is equal to £{total_bet}")
 
 main()
