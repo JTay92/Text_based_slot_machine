7f8a1db384d9db0a381a9e1dc3bd2473164f9b98
JTay92 <joe.r.tayler@outlook.com>
2023-03-25 17:14:54 +0000

    added deposit function for user amount

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+def deposit():
+    while True: # Loop runs until valid number is provided
+        amount = input("What would you like to deposit? £ ")
+        if amount.isdigit():    # Check inputted amount is a number
+            amount = int(amount)    # Convert amount from string to integer
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0!")
+        else:
+            print("Please enter a number.")
+    
+    return amount
+
+deposit()
